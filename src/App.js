0f8a487d84f1351d52fd69ea4188b8340832af3e
film_negativa <aekapatc@gmail.com>
2021-06-20 16:20:26 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import useAxios from "axios-hooks";
 
 function App() {
+  const [{ data, loading, error }, refetch] = useAxios("https://api.publicapis.org/categories");
+  const [items, setItem] = useState([]);
+  const [txtFilter , setTxtFilter] = useState('')
+  const [showData , setShowdata ] = useState([])
+
+  useEffect(() => {
+    setItem(data)
+    setShowdata(data);
+  }, [data])
+
+  useEffect(() => {
+    if(txtFilter !== '') {
+      let regString = `${txtFilter}`;
+      let regexp = new RegExp(regString, "i");
+      const result = items.filter((r) => {
+        if (regexp.test(r)) return r;
+      });
+      setShowdata(result);
+    } else {
+      setShowdata(items);
+    }
+  }, [txtFilter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          value={txtFilter}
+          onChange={(e) => {
+            setTxtFilter(e.target.value);
+          }}
+        />
+      </div>
+      <table style = {{paddingTop : '10px'}}>
+        {showData ? showData.map((val) =>
+        <tbody><div style={{border: '1px solid #dddddd' , width :'15rem'}} key={val}> {val}</div></tbody>) : null}
+      </table>
     </div>
   );
 }
